bfd3a5a7e0ed159a09efd41229ae86fb4720eb64
wuyeelok <wuyeelok@gmail.com>
2019-02-18 14:36:26 +0800

    Simple animation expend to right and return back to normal

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,10 +3,11 @@ const lineOne = hamburger.querySelector('.line-one')
 const lineTwo = hamburger.querySelector('.line-two')
 const lineThree = hamburger.querySelector('.line-three')
 
+const lines = [lineOne, lineTwo, lineThree]
+
 const tlm = new TimelineMax({});
 
 hamburger.addEventListener('mouseenter', _ => {
-    tlm
-        .to(lineOne, .25, {scaleX: 1.5})
-        .to(lineOne, .25, {scaleX: 1})
+    tlm.staggerTo(lines, 0.25, {scaleX: 1.5, repeat: 1, yoyo: true}, 0.125)
 })
+
